0e9dd86f56c1a1119a315ddd89bb7ffdcf28a982
Zachary Allnutt <zachary.allnutt@invensys.com>
2017-05-03 15:03:04 -0700

    Wrapping up for the work day

--- CourseProject/src/app/app.module.ts
+++ CourseProject/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { HeaderComponent } from './header/header.component';
+import { AppComponent } from './App/app.component';
+import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './ShoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './RecipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './RecipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './RecipeBook/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './RecipeBook/recipes/recipes.component';
+import { HeaderComponent } from './Header/header.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { HeaderComponent } from './header/header.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
